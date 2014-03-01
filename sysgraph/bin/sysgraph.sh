bd4fe57edbb175e88c9aacea6875268d62e4ac92
Vasilis Ventirozos <vventirozos@omniti.com>
2014-03-01 13:45:12 +0000

    kek

--- sysgraph/bin/sysgraph.sh
+++ sysgraph/bin/sysgraph.sh
@@ -4,15 +4,16 @@ source ../etc/sysgraph.conf
 function help {
 echo "usage: ./sysgraph.sh <date> <graph type>"
 echo "example: ./sysgraph.sh 2013-10-22 cpu_load"
-echo "currently available graphs : \
-cpu_load \
-cpu_pct \
-io_read \
-io_write \
-io_wait \
-mem_used \
-active_conn \
-locks"
+echo "currently available graphs :"
+echo ""
+echo "cpu_load"
+echo "io_read"
+echo "io_write"
+echo "io_wait"
+echo "mem_used"
+echo "active_conn"
+echo "pg_shared_buffers"
+echo ""
 }
 if test $# -ne 2; then
 	help
@@ -30,33 +31,31 @@ if [ $graph = "cpu_load" ]
 	colums="1:2"
 	limit="100"
 	plot_command="plot '$tmpdir/output.csv' using 1:2 title '%user' with lines ls 1 , '$tmpdir/output.csv' using 1:3 title '%system' with lines ls 2,'$tmpdir/output.csv' using 1:4 title '%idle' with lines ls 3"
-elif [ $graph = cpu_pct ]
-	then file_pattern="mpstat"
 elif [ $graph = io_read ]
 	then
 	title="io read report for $date"
 	xlabel="time"
-	ylabel="reads"
+	ylabel="KB"
 	colums="1:2"
 	limit="10000"
 	file_pattern="iostat"
-	plot_command="plot '$tmpdir/output.csv' using 1:2 title 'writes' with lines ls 1"		                  
+	plot_command="plot '$tmpdir/output.csv' using 1:2 title 'Reads KB' with lines ls 1"		                  
 elif [ $graph = io_write ]
 	then file_pattern="iostat"
 	title="io write report for $date"
 	xlabel="time"
-	ylabel="writes"
+	ylabel="KB"
 	colums="1:2"
-	limit="80000"
-        plot_command="plot '$tmpdir/output.csv' using 1:2 title 'writes' with lines ls 1"
+	limit="600000"
+    plot_command="plot '$tmpdir/output.csv' using 1:2 title 'Writes KB' with lines ls 1"
 elif [ $graph = io_wait ]
 	then file_pattern="iostat"
 	title="IO wait for $date"
 	xlabel="Time"
 	ylabel="Wait"
 	colums="1:2"	
-        limit="2"
-        plot_command="plot '$tmpdir/output.csv' using 1:2 title 'writes' with lines ls 1"
+    limit="2"
+    plot_command="plot '$tmpdir/output.csv' using 1:2 title 'writes' with lines ls 1"
 elif [ $graph = mem_used ]
 	then file_pattern="mem"
 	title="Used Memory for $date"
@@ -64,23 +63,31 @@ elif [ $graph = mem_used ]
 	ylabel="Used"
 	colums="1:2"	
 	limit="128"
-        plot_command="plot '$tmpdir/output.csv' using 1:2 title 'Memory used (MB)' with lines ls 1"
+    plot_command="plot '$tmpdir/output.csv' using 1:2 title 'Memory used (MB)' with lines ls 1"
 elif [ $graph = active_conn ]    
 	then file_pattern="pg_stat_activity"
 	title="active_connections for $date"
 	xlabel="Time"
 	ylabel="connections"
 	colums="1:2" 
-	limit="20"                                                                                        
-        plot_command="plot '$tmpdir/output.csv' using 1:2 title 'Locks' with lines ls 1"         
-elif [ $graph = locks ]    
-	then file_pattern="pg_locks"
-	title="locks for $date"
+	limit="100"                                                                                        
+    plot_command="plot '$tmpdir/output.csv' using 1:2 title 'connection count' with lines ls 1"         
+elif [ $graph = pg_shared_buffers ]
+	then file_pattern="pg_stat_bgwriter"
+	title="backend buffers for $date"
 	xlabel="Time"
-	ylabel="locks"  
+	ylabel="buffers"
 	colums="1:2" 
-limit="20"                 
-                                                
+	limit="1000000000"                                                                                        
+    plot_command="plot '$tmpdir/output.csv' using 1:2 title 'buffer count' with lines ls 1"         
+#elif [ $graph = exclusive_locks ]    
+#	then file_pattern="pg_locks"
+#	title="locks for $date"
+#	xlabel="Time"
+#	ylabel="exclusive locks"  
+#	colums="1:2" 
+#    plot_command="plot '$tmpdir/output.csv' using 1:2 title 'exlusive locks' with lines ls 1"
+#	limit="20"                                                        
 else 
 	echo "not right graph type !!"
 	exit 0
@@ -102,8 +109,6 @@ gunzip $tmpdir/*.gz
 function _run_parser {
 if [ $graph = "cpu_load" ]
 	then $parsedir/cpu_usage.sh 
-elif [ $graph = cpu_pct ]
-	then $parsedir/cpu_usage.sh
 elif [ $graph = io_read ]
 	then $parsedir/reads_writes.sh $tmpdir $tmpdir $read_disk $read_column $read_op
 elif [ $graph = io_write ]
@@ -114,9 +119,8 @@ elif [ $graph = mem_used ]
 	then $parsedir/memory_used.sh
 elif [ $graph = active_conn ]
         then $parsedir/connection_count.sh $tmpdir $tmpdir
-elif [ $graph = active_conn ]
-        then $parsedir/locks.sh $tmpdir $tmpdir
-        
+elif [ $graph = pg_shared_buffers ]
+        then $parsedir/pg_shared_buffers.sh $tmpdir $tmpdir
 else 
 	echo "no parser :("
 fi
@@ -142,16 +146,14 @@ set xrange[ "00:00:00":"23:59:59" ]
 set yrange [ 0:"$limit" ]
 set format x "%H:%M"
 set datafile separator ','
-set style line 1 lc rgb '#0000000' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
-set style line 3 lc rgb '#0000000' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
-set style line 4 lc rgb '#FF0000' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
-set style line 1 lc rgb '#708090' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
-set style line 2 lc rgb '#8B0000' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
-#plot '$tmpdir/output.csv' using 1:2 title 'writes' with lines ls 1 , '$tmpdir/output.csv' using 1:3 title 'reads' with lines ls 2
+set style line 1 lc rgb '#1E90FF' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
+set style line 4 lc rgb '#32CD32' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
+set style line 5 lc rgb '#191970' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
+set style line 2 lc rgb '#FA8072' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
+set style line 3 lc rgb '#B22222' lt 10 lw 0.5 pt 1 pi -1 ps 0.5
 $plot_command
 EOF
 
 echo "Cleaning up temp dir .."
-#cp $tmpdir/output.csv ~/
-rm -rf $tmpdir/*
+#rm -rf $tmpdir/*
 echo "Your file can be found at $graphdir/$pngfile ... enjoy"
